ec6c429e4d7504acdcc5da5c03d61c9f6054e779
Patrick Nunes <pj.nunes.dy@gmail.com>
2025-10-11 13:21:29 -0400

    unfinished loop bug fixing 1

---
+++ unfinished_loop_bug_fixing_1.py
@@ -0,0 +1,10 @@
+# def create_array(n):
+#     res=[]
+#     i=0
+#     while i< n:
+#         i+=1
+#         res+=[i]
+#     return res
+
+def create_array(n):
+    return [i for i in range(1, n+1)]
