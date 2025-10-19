cafce2c4db66aed4ce7f72cd81e3e0f84441bc70
Patrick Nunes <pj.nunes.dy@gmail.com>
2025-10-18 23:45:43 -0400

    bingo card

--- Bingo_card.py
+++ Bingo_card.py
@@ -2,25 +2,17 @@ import random
 
 def get_bingo_card():
     
-    columns = {
-        "B": random.sample(range(1, 15), 5),
-        "I": random.sample(range(16, 30), 5),
-        "N": random.sample(range(31, 45), 4),
-        "G": random.sample(range(46, 60), 5),
-        "O": random.sample(range(61, 75), 5)
+    card = {
+        "B": random.sample(range(1, 16), 5),
+        "I": random.sample(range(16, 31), 5),
+        "N": random.sample(range(31, 46), 4),
+        "G": random.sample(range(46, 61), 5),
+        "O": random.sample(range(61, 76), 5)
     }
 
 
     bingo_card = []
-
-    for i in range(5):
-        row = []
-        for col in ["B", "I", "N", "G", "O"]:
-            if col == "N" and i == 2:
-                row.append("FREE")
-            else:
-                row.append(columns[col][i if col != "N" or i < 2 else i - 1])
-        bingo_card.append(row)
+    for letter in "BINGO":
+        for number in card[letter]:
+            bingo_card.append(f"{letter}{number}")  
     return bingo_card
-
-
