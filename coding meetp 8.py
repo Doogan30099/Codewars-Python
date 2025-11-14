b352f141865abf84c81711e908001b95621f983d
Patrick Nunes <pj.nunes.dy@gmail.com>
2025-11-13 19:19:39 -0500

    coding meetup 8

---
+++ coding meetp 8.py
@@ -0,0 +1,7 @@
+def all_continents(lst): 
+    continents = {'Africa', 'Americas', 'Asia', 'Europe', 'Oceania'}
+    devloc = {i['continent'] for i in lst}
+    if continents == devloc: 
+        return True 
+    return False
+    
